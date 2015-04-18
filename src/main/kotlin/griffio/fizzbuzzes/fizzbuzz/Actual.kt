17aadda5070f5f5828ba90118edd96fbd54699e8
griffio <griffio@users.noreply.github.com>
2015-04-18 22:05:55 +0100

    fizzbuzz

--- src/main/kotlin/griffio/fizzbuzzes/fizzbuzz/Actual.kt
+++ src/main/kotlin/griffio/fizzbuzzes/fizzbuzz/Actual.kt
@@ -1,14 +1,36 @@
 package griffio.fizzbuzzes.fizzbuzz
 
 fun main(args: Array<String>): Unit {
-    for (i in 1..100) {
-        println(
-                when {
-                    i mod 15 == 0 -> "FizzBuzz"
-                    i mod 3 == 0 -> "Fizz"
-                    i mod 5 == 0 -> "Buzz"
-                    else -> "$i"
-                }
-        )
+
+    fun one() {
+
+        for (i in 1..100) {
+            println(
+                    when {
+                        i mod 15 == 0 -> "FizzBuzz"
+                        i mod 3 == 0 -> "Fizz"
+                        i mod 5 == 0 -> "Buzz"
+                        else -> "$i"
+                    }
+            )
+        }
+
     }
+
+
+    fun two() {
+
+        for (i in 1..100) {
+            println(
+                    if (i % 15 == 0) "FizzBuzz"
+                    else if (i % 3 == 0) "Fizz"
+                    else if (i % 5 == 0) "Buzz"
+                    else i.toString())
+        }
+    }
+
+    one()
+
+    two()
+
 }
