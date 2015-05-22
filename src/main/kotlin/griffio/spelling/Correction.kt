3a55a6455fd4e795de7f2d24d0042f3478da1b29
griffio <griffio@users.noreply.github.com>
2015-05-22 17:12:34 +0100

    updates

--- src/main/kotlin/griffio/spelling/Correction.kt
+++ src/main/kotlin/griffio/spelling/Correction.kt
@@ -8,29 +8,35 @@ import com.google.common.io.Files
 import com.google.common.io.Resources
 import com.google.common.collect.Multiset
 import java.io.File
+import java.net.URI
+import java.net.URL
+import java.nio.file.Paths
 import java.util.HashSet
 import java.util.Locale
 import java.util.Scanner
 
-// http://norvig.com/spell-correct.html
-fun main(args: Array<String>) {
+class Correction(var resource : String) {
+
+    var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    var wordsN = train(words())
 
     fun List<String>.or(a: List<String>): List<String> {
         return if (this.isNotEmpty()) this else a
     }
 
-    fun words(resource: String): String {
-        return Resources.asCharSource(Resources.getResource(resource), Charsets.UTF_8).read().toLowerCase(Locale.ROOT)
+    fun loadResource(): URL {
+        return this.javaClass.getResource(resource)
+    }
+
+    fun words(): String {
+        return loadResource().readText(charset = Charsets.ISO_8859_1)
     }
 
     fun train(words: String): HashMultiset<String> {
         val alphas = Splitter.on(CharMatcher.WHITESPACE).trimResults(CharMatcher.inRange('a', 'z').negate())
         return HashMultiset.create(alphas.split(words))
     }
-
-    var wordsN = train(words("big.txt"))
-
-    var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     //    //splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     //    //deletes    = [a + b[1:] for a, b in splits if b]
@@ -39,7 +45,7 @@ fun main(args: Array<String>) {
     //    //inserts    = [a + c + b     for a, b in splits for c in alphabet]
 
     fun edits1(word: String): Set<String> {
-        var splits = IntRange(0, word.length()).map { it -> Pair(word.take(it), word.drop(it)) }
+        var splits = IntRange(0, word.length()) map { it -> Pair(word.take(it), word.drop(it)) }
         var edits1 = hashSetOf<String>()
         splits.filter { it -> it.second.isNotEmpty() }.mapTo(edits1) { it -> it.first.concat(it.second.substring(1)) }
         splits.filter { it -> it.second.length() > 1 }.mapTo(edits1) { it -> it.first + it.second.get(1) + it.second.get(0) + it.second.substring(2) }
@@ -55,7 +61,7 @@ fun main(args: Array<String>) {
 
     //set(w for w in words if w in NWORDS)
     fun known(words: List<String>): List<String> {
-        return words filter { word -> wordsN.contains(word) }
+        return words filter { word -> wordsN contains(word) }
     }
 
     //candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
@@ -65,6 +71,9 @@ fun main(args: Array<String>) {
         return candidates.maxBy { wordsN.count(it) }.orEmpty()
     }
 
-    println(correct("clurk")) //clerk
+}
 
+// http://norvig.com/spell-correct.html
+fun main(args: Array<String>) {
+    println(Correction("/big.txt") correct("transparen")) //transparent
 }
