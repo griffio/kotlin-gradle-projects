0f46498c59eaa4cb01280bbae09ca98cf1922e55
griffio <griffio@users.noreply.github.com>
2016-05-13 12:51:14 +0100

    PathTest
    backticks and infix operators

---
+++ src/test/kotlin/griffio/PathTest.kt
@@ -0,0 +1,18 @@
+package griffio
+
+import org.junit.Test
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+
+class PathTest {
+
+    infix operator fun Path.div(arg: String) = this.resolve(arg)
+
+    @Test
+    fun `resolves the path to user preferences`() {
+        val dataPath = Paths.get("/data")
+        val userPreferences = dataPath / "user" / "preferences.json"
+        assertEquals("/data/user/preferences.json", userPreferences.toString())
+    }
+}
