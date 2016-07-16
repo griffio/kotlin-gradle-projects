d8489f71bc99a794f2f2ce66c04bf6bb1a411797
griffio <griffio@users.noreply.github.com>
2016-07-16 02:14:52 +0100

    Arrays

--- src/main/kotlin/griffio/things/Arrays.kt
+++ src/main/kotlin/griffio/things/Arrays.kt
@@ -1,16 +1,15 @@
 package griffio.things
 
-inline fun <reified T> matrix(height:Int, width:Int, noinline init: (Int, Int) -> Array<T>) = Array<Array<T>>(height, {row-> init(row, width)})
+inline fun <reified T> matrix2d(height: Int, width: Int, init: (Int, Int) -> Array<T>) = Array<Array<T>>(height, { row -> init(row, width) })
 
 fun main(args: Array<String>) {
-    val stringTable = matrix(5, 10, { row:Int, width:Int -> Array(width) {col-> "|$row$col"} })
 
-    for (rows in stringTable) {
-        for (row in rows) {
-            for (cell in row) {
-                print(cell)
-            }
+    val table = matrix2d(5, 10, { row: Int, width: Int -> Array(width) { col -> "|$row$col" } })
+
+    for (cells in table) {
+        for (cell in cells) {
+            print(cell)
         }
-        println("|\n")
+        println("|")
     }
 }
