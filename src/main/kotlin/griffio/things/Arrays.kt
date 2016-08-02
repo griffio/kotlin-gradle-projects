432e1a244e081f853d15e036179a938bc11642f4
griffio <griffio@users.noreply.github.com>
2016-08-02 18:27:53 +0100

    helical2d

--- src/main/kotlin/griffio/things/Arrays.kt
+++ src/main/kotlin/griffio/things/Arrays.kt
@@ -12,4 +12,73 @@ fun main(args: Array<String>) {
         }
         println("|")
     }
+
+    helical2d()
+
+}
+
+enum class HelicalDirection {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+}
+
+fun helical2d() {
+
+    val rc: Array<CharArray>
+    rc = arrayOf(
+            charArrayOf('A', 'B', 'C', 'D'),
+            charArrayOf('L', 'M', 'N', 'E'),
+            charArrayOf('K', 'P', 'O', 'F'),
+            charArrayOf('J', 'I', 'H', 'G'))
+
+    val lr = object {
+        var left: Int = 0
+        var right: Int = 0
+    }
+
+    val tb = object {
+        var top: Int = 0
+        var bottom: Int = 0
+    }
+
+    lr.right = rc.size - 1
+    tb.bottom = rc[0].size - 1
+
+    var direction = HelicalDirection.RIGHT
+
+    while (lr.left <= lr.right && tb.top <= tb.bottom) {
+
+        when (direction) {
+            HelicalDirection.RIGHT -> {
+                for (i in lr.left..lr.right) {
+                    println(rc[tb.top][i])
+                }
+                tb.top++
+                direction = HelicalDirection.DOWN
+            }
+            HelicalDirection.DOWN -> {
+                for (i in tb.top..tb.bottom) {
+                    println(rc[i][lr.right])
+                }
+                lr.right--
+                direction = HelicalDirection.LEFT
+            }
+            HelicalDirection.LEFT -> {
+                for (i in lr.right downTo lr.left) {
+                    println(rc[tb.bottom][i])
+                }
+                tb.bottom--
+                direction = HelicalDirection.UP
+            }
+            HelicalDirection.UP -> {
+                for (i in tb.bottom downTo tb.top) {
+                    println(rc[i][lr.left])
+                }
+                lr.left++
+                direction = HelicalDirection.RIGHT
+            }
+        }
+    }
 }
