74ffa39bd531314eabec283410b66266af785072
griffio <griffio@users.noreply.github.com>
2016-09-18 17:40:56 +0100

    Fizzbuzz ADT

--- src/main/kotlin/griffio/fizzbuzzes/fizzbuzz/Actual.kt
+++ src/main/kotlin/griffio/fizzbuzzes/fizzbuzz/Actual.kt
@@ -1,36 +1,87 @@
 package griffio.fizzbuzzes.fizzbuzz
 
+sealed class FizzBuzzGame(val i: Int) {
+
+  companion object {
+    operator fun invoke(i: Int): FizzBuzzGame {
+      return when {
+        i.mod(15) == 0 -> FizzBuzzGame.FizzBuzz(i)
+        i.mod(3) == 0 -> FizzBuzzGame.Fizz(i)
+        i.mod(5) == 0 -> FizzBuzzGame.Buzz(i)
+        else -> FizzBuzzGame.Digit(i)
+      }
+    }
+  }
+
+  override fun toString() = i.toString()
+
+  class Fizz(i: Int) : FizzBuzzGame(i) {
+    override fun toString() = "Fizz"
+  }
+
+  class Buzz(i: Int) : FizzBuzzGame(i) {
+    override fun toString() = "Buzz"
+  }
+
+  class FizzBuzz(i: Int) : FizzBuzzGame(i) {
+    override fun toString() = "FizzBuzz"
+  }
+
+  class Digit(i: Int) : FizzBuzzGame(i)
+}
+
 fun main(args: Array<String>): Unit {
 
-    fun one() {
+  fun one() {
 
-        for (i in 1..100) {
-            println(
-                    when {
-                        i.mod(15) == 0 -> "FizzBuzz"
-                        i.mod(3) == 0 -> "Fizz"
-                        i.mod(5) == 0 -> "Buzz"
-                        else -> "$i"
-                    }
-            )
-        }
+    for (i in 1..100) {
+      println(
+          when {
+            i.mod(15) == 0 -> "FizzBuzz"
+            i.mod(3) == 0 -> "Fizz"
+            i.mod(5) == 0 -> "Buzz"
+            else -> "$i"
+          }
+      )
+    }
+  }
 
+  fun two() {
+
+    for (i in 1..100) {
+      println(
+          if (i % 15 == 0) "FizzBuzz"
+          else if (i % 3 == 0) "Fizz"
+          else if (i % 5 == 0) "Buzz"
+          else i.toString())
+    }
+  }
+
+  fun three() {
+
+    (1..100).forEach {
+      println(FizzBuzzGame(it))
     }
 
+  }
 
-    fun two() {
+  fun four() {
 
-        for (i in 1..100) {
-            println(
-                    if (i % 15 == 0) "FizzBuzz"
-                    else if (i % 3 == 0) "Fizz"
-                    else if (i % 5 == 0) "Buzz"
-                    else i.toString())
-        }
-    }
+    print("Only FizzBuzz")
 
-    one()
+    (1..100)
+        .map { FizzBuzzGame(it) }
+        .filter { it is FizzBuzzGame.FizzBuzz }
+        .forEach { println("$it on ${it.i}") }
+  }
 
-    two()
+  one()
 
+  two()
+
+  three()
+
+  four()
 }
+
+
