93d0bc362f259805f0b822f5ffd7f17b52774a43
griff і⊙ <griffio@users.noreply.github.com>
2017-01-02 21:29:48 +0000

    Update Big.kt

--- src/main/kotlin/griffio/spelling/Big.kt
+++ src/main/kotlin/griffio/spelling/Big.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
 
     val url = URL("http://norvig.com/big.txt")
 
+    // default is 8k buffer
     val maxLine = url.openStream().bufferedReader().useLines { it.maxBy(String::length) }
 
     println("maxLine = $maxLine")
